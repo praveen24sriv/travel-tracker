8b0e35e81d4fa65dddfd00194d6b946bfa7aa2cb
PRAVEEN RAJ SRIVASTAV <154606949+praveen24sriv@users.noreply.github.com>
2024-10-02 15:24:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "NEEVarp15",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// GET home page
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT country_codes FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_codes);
+  });
+  console.log(result.rows);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
 
